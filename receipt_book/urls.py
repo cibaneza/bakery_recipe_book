c28748938941cc6ba7150f5d86673bbd5637ea19
cibaneza <cibaneza@uft.edu>
2024-03-01 23:01:11 -0300

    Conflicts resolve

--- receipt_book/urls.py
+++ receipt_book/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import home, receiptcosts, product_detail, delete_product_ingredient, update_product_ingredient, get_product_ingredient_data, get_ingredients, add_product_ingredient, ingredients, get_ingredient_units, download_product_data_excel, download_ingredients_data_excel, download_products_with_ingredients
 
 urlpatterns = [
+    path('', home, name="home"),
     path('receipt_home', home, name="receipt-home"),
     path('receipt_costs', receiptcosts, name="receipt-costs"),
     path('recipe_book/<int:product_id>', product_detail, name="product_detail"),
